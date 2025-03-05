6f6202340318b222854dfb7d4f97ae2e69cd7d4c
Victor Rutskin <lashaka666@gmail.com>
2025-03-05 11:40:39 +0200

    probability class, priority level enum & project restructure
    - added models folder for cleaner file structure
    - added priority level enum
    - initial work on probabillity class

--- Logic/MainLogic.cs
+++ Logic/MainLogic.cs
@@ -1,16 +1,18 @@
-﻿namespace Logic
+﻿using Logic.Models;
+
+namespace Logic
 {
     public static class MainLogic
     {
         public static List<Item> items = new();
 
-        private const int _minPriorityValue = 1;
-        private const int _maxPriorityValue = 3;
+        private const int _minPriorityValue = (int)PriorityLevel.High;
+        private const int _maxPriorityValue = (int)PriorityLevel.Low;
         private const int _itemsPerPriority = 5;
 
+        // Populate the list with initial items, 5 items per priority, 3 priorities, 15 items total
         public static void PopulateInitialItems()
         {
-
             for (int priority = _minPriorityValue; priority <= _maxPriorityValue; priority++)
             {
                 for (int i = 0; i < _itemsPerPriority; i++)
---
+++ Logic/Probability.cs
@@ -0,0 +1,15 @@
+﻿using Logic.Models;
+namespace Logic
+{
+    class Probability
+    {
+        private readonly Dictionary<PriorityLevel, double> _probabilities = new()
+        {
+            { PriorityLevel.High, 0.6 },
+            { PriorityLevel.Medium, 0.3 },
+            { PriorityLevel.Low, 0.1 }
+        };
+
+       
+    }
+}
